80f68451b86f3ae902c33a9bb8ea3cd123799353
Spencer Cloughley <spencercloughley@gmail.com>
2023-11-29 18:53:30 -0500

    Added css styling and an image to the stage icon

--- src/main/java/com/example/comp1011assignment2/Main.java
+++ src/main/java/com/example/comp1011assignment2/Main.java
@@ -3,6 +3,7 @@ package com.example.comp1011assignment2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("now-playing-list-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("imgs/Icon.jpg")));
         stage.setTitle("Movies in Theaters!");
         stage.setScene(scene);
         stage.show();
